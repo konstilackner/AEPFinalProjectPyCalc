cd0ba5ec37f981b8b3ebd467e87c1848fc9f706d
konstilackner <konstantin.lackner@onlinehome.de>
2016-04-29 14:26:13 -0700

    Sixth Commit

--- TestCalculator.py
+++ TestCalculator.py
@@ -31,11 +31,11 @@ class TestCalculator(unittest.TestCase):
   def test_FiveTimesFiveIsTwentyfive(self):
       five = Calculator(5)
       self.assertEqual(25, five.multiply(5))
-      """   
+      
   def test_ZeroDividedByTenIsZero(self):
       zero = Calculator(0)
       self.assertEqual(0, zero.divide(10))
-      
+      """
   def test_SevenDividedByOneIsSeven(self):
       seven = Calculator(7)
       self.assertEqual(7, seven.divide(1))
